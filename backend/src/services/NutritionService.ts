2caeab3df27e71a19da82aff2874faf7897382ba
hugogacia360 <hhugokta@hotmail.com>
2024-10-30 11:21:00 -0300

    installed googlegenerative-ai

--- backend/src/services/NutritionService.ts
+++ backend/src/services/NutritionService.ts
@@ -1,10 +1,14 @@
 import { DataProps } from "../controllers/NutritionController"
+import {GoogleGenerativeAI} from "@google/generative-ai"
 
 class NutritionService{
     async execute({name, age, gender, weight, height, objective, level}: DataProps){
-        console.log("TESTE")
+        try{
 
-        return {message:"SERVICE CHAMADO"}
+        }catch(err){
+            console.log("Erro JSON", err)
+            throw new Error("Falha")
+        }
     }
 }
 
