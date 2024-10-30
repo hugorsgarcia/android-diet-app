ceeb23a7329eb31c03c11935d6ab4ffc3b7d1481
hugogacia360 <hhugokta@hotmail.com>
2024-10-30 11:41:20 -0300

    testing gemini api

--- backend/src/services/NutritionService.ts
+++ backend/src/services/NutritionService.ts
@@ -4,7 +4,14 @@ import {GoogleGenerativeAI} from "@google/generative-ai"
 class NutritionService{
     async execute({name, age, gender, weight, height, objective, level}: DataProps){
         try{
-            
+            const genAI = new GoogleGenerativeAI(process.env.API_KEY!)
+            const model = genAI.getGenerativeModel({model: "gemini-1.5-flash"})
+
+            const response = await model.generateContent("Em que ano o C++ foi criado");
+            console.log(JSON.stringify(response, null, 2));
+            return {ok: true}
+
+
         }catch(err){
             console.log("Erro JSON", err)
             throw new Error("Falha")
