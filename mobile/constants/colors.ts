dcc6f02acc3c376a6ba73528d5ba177ab9afc0e1
hugogacia360 <hhugokta@hotmail.com>
2024-11-02 18:36:11 -0300

    app home page

--- mobile/constants/colors.ts
+++ mobile/constants/colors.ts
@@ -3,5 +3,10 @@ const tintColorLight = '#0a7ea4';
 const tintColorDark = '#fff';
 
 export const colors = {
-  
+    background: '#0F232C',
+    white: '#FFF',
+    green: '#1EB500',
+    orange: '#F79600',
+    blue: '#009AD6',
+    black: '#000'
 };
